bfce8052b95d0177b4e685fa416e850eacbb8910
Dimitri RAMANANTSOA <dimitri.rama.pro@gmail.com>
2022-08-08 20:52:47 +0200

    create custom DataPersister

--- src/DataProvider/DependencyDataProvider.php
+++ src/DataProvider/DependencyDataProvider.php
@@ -4,29 +4,19 @@ namespace App\DataProvider;
 
 use Ramsey\Uuid\Uuid;
 use App\Entity\Dependency;
+use App\Repository\DependencyRepository;
 use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 
 class DependencyDataProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface, ItemDataProviderInterface
 {
-    public function __construct(private string $rootPath){}
 
-    private function getDependencies() : array
-    {
-        $path = $this->rootPath . '/composer.json';
-        $json = json_decode(file_get_contents($path), true);
-        return $json['require'];
-    }
+    public function __construct(private DependencyRepository $repository){}
 
     public function getCollection(string $ressourceClass, string $operationName = null, array $context = [])
     {
-        $items = [];
-        foreach ($this->getDependencies() as $name => $version)
-        {
-            $items[] = new Dependency(Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString(), $name, $version);
-        }
-        return $items;
+        return $this->repository->findAll();
     }
 
     public function supports(string $ressourceClass, string $operationName = null, array $context = []) : bool
@@ -36,16 +26,7 @@ class DependencyDataProvider implements CollectionDataProviderInterface, Restric
 
     public function getItem (string $ressourceClass, $id, string $operationName = null, array $context = [])
     {
-        $dependencies = $this->getDependencies();
-        foreach ($dependencies as $name => $version)
-        {
-            $uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
-            if ($uuid === $id)
-            {
-                return new Dependency($uuid, $name, $version);
-            }
-        }
-
-        return null;
+        return $this->repository->find($id);
     }
+    
 }
--- src/Entity/Dependency.php
+++ src/Entity/Dependency.php
@@ -1,12 +1,24 @@
 <?php
 namespace App\Entity;
 
+use Ramsey\Uuid\Uuid;
 use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
-    itemOperations: ['get'],
-    collectionOperations: ['get'],
+    itemOperations: [
+        'get', 
+        'put' =>[
+            'denormalization_context' => [
+                'groups' => ['put:Dependency']
+            ]
+            ],
+            'delete'
+    ],
+    collectionOperations: ['get','post'],
     paginationEnabled: false
 )]
 class Dependency
@@ -18,7 +30,10 @@ class Dependency
 
     #[ApiProperty(
         description: 'Nom de la dépendance'
-    )]
+    ),
+        Length(min:2),
+        NotBlank()
+    ]
     private string $name;
 
     #[ApiProperty(
@@ -26,12 +41,16 @@ class Dependency
         openapiContext: [
             'example' => "^3.2"
         ]
-    )]
+    ),
+        Length(min:2),
+        NotBlank(),
+        Groups(['put:Dependency'])
+    ]
     private string $version;
 
-    public function __construct(string $uuid, string $name, string $version)
+    public function __construct(string $name, string $version)
     {
-        $this->uuid = $uuid;
+        $this->uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
         $this->name = $name;
         $this->version = $version;
     }
@@ -64,5 +83,20 @@ class Dependency
     public function getVersion(): string
     {
         return $this->version;
+    }
+
+
+    /**
+     * Set the value of version
+     *
+     * @param string $version
+     *
+     * @return self
+     */
+    public function setVersion(string $version): self
+    {
+        $this->version = $version;
+
+        return $this;
     }
 }
